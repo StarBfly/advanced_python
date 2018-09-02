76b7ad74e33e4f99910c368dce35fa0c0fdb9924
StarBfly <margot.blackwood@gmail.com>
2018-09-03 01:01:30 +0300

    Pycodestyle changes

--- homework_2.py
+++ homework_2.py
@@ -3,15 +3,18 @@
 import sys
 
 # Getting default recursion limit
-print(f"Current recursion depth limit is : {sys.getrecursionlimit()}")
+print(sys.getrecursionlimit())
 
 # Setting a higher recursion depth by bitwise shift to cause C stack overflow
 sys.setrecursionlimit(1 << 20)
 
-print(f"New recursion depth limit is: {sys.getrecursionlimit()}")
+print(sys.getrecursionlimit())
+
 
 # Initializing recursion
-recurse_func = lambda recurse_func: recurse_func(recurse_func)
+def recurse_func(recurse_func):
+    recurse_func(recurse_func)
+
 
 if __name__ == "__main__":
     recurse_func(recurse_func)
